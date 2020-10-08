3d2abd7214cd443beeaec278a1a7397ba3ba0e54
Arnob Chowdhury <as9025@srmist.edu.in>
2020-10-08 09:32:23 +0530

    reverse geocode pogo fixed

--- covid/src/main/java/com/covid_booking/covid/model/Addresscontainer.java
+++ covid/src/main/java/com/covid_booking/covid/model/Addresscontainer.java
@@ -1,18 +1,49 @@
 package com.covid_booking.covid.model;
 
 import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Addresscontainer {
+    private String streetName;
+    private String municipality;
+    private String countrySubdivisionName;
+    private String postalCode;
+    private String country;
     private List<Address> addresses;
 
-    public List<Address> getAddresses() {
-        return addresses;
-    }
 
     public void setAddresses(List<Address> addresses) {
         this.addresses = addresses;
     }
 
+    public String getStreetName() {
+        return addresses.get(0).getStreetName();
+    }
 
     
+
+    public String getMunicipality() {
+        return addresses.get(0).getMunicipality();
+    }
+
+   
+
+    public String getCountrySubdivisionName() {
+        return addresses.get(0).getCountrySubdivisionName();
+    }
+
+    
+
+    public String getPostalCode() {
+        return addresses.get(0).getPostalCode();
+    }
+
+    
+
+    public String getCountry() {
+        return addresses.get(0).getCountry();
+    }
+
 }
